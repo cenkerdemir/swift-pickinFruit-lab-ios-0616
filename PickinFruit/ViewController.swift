a27527af9a148ed64ee8d767bce1cafaf86195f8
github username <github email address>
2016-07-25 17:10:22 -0400

    uipickerview with delegates and protocols

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,24 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
+    let numberOfRows = 9999
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
-        self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.fruitPicker.delegate = self
+        self.fruitPicker.dataSource = self
         
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
+        var selections: [String] = []
+        for i in 0..<3 {
+            let rowToShow = Int(arc4random_uniform(UInt32(numberOfRows)))
+            fruitPicker.selectRow(rowToShow, inComponent: i, animated: true)
+            if let symbol = self.pickerView(self.fruitPicker, titleForRow: rowToShow, forComponent: i) {
+                selections.append(symbol)
+            }
+        }
         
+        if self.isThereAWinner(selections) {
+            self.resultLabel.text = "WINNER!"
+            self.animateLabel(self.resultLabel, repeatCount: 16.0)
+        }
+        else {
+            self.resultLabel.text = "TRY AGAIN"
+            self.animateLabel(self.resultLabel, repeatCount: 2.0)
+        }
+    }
+    
+    func animateLabel(label: UILabel, repeatCount: Float) {
         
+        UIView.animateWithDuration(0.3, delay: 0.0, options: [.Repeat/*, .Autoreverse*/], animations: {
+            UIView.setAnimationRepeatCount(repeatCount)
+            label.alpha = 1.0
+            label.alpha = 0.0
+            }, completion: nil)
+    }
+    
+    func pickerView(pickerView: UIPickerView,
+                      numberOfRowsInComponent component: Int) -> Int {
+        return self.numberOfRows
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row % self.fruitsArray.count]
+    }
+    
+    func isThereAWinner(selections:[String]) -> Bool{
+        if selections[0] == selections[1] && selections[0] == selections[2] {
+            return true
+        }
+        else {
+            return false
+        }
     }
     
 }
